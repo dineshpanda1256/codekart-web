5c2304fee7e23672de10ca0da0acdfd3c07581ee
Dinesh <dineshpanda1256@gmail.com>
2023-04-19 11:15:14 +0530

    header,footeradded

--- src/navigations/RootRouter.js
+++ src/navigations/RootRouter.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { NavConstants } from "./NavConstants";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import Footer from "../components/Footer/Footer";
+import Header from "../components/Header/Header";
 
 export default function RootRouter() {
 
@@ -11,9 +13,11 @@ export default function RootRouter() {
     <>
     <ToastContainer />
       <Router>
+        <Header/>
         <Routes>
           <Route path={NavConstants.home} element={<Home />} />
         </Routes>
+        <Footer/>
       </Router>
     </>
   );
